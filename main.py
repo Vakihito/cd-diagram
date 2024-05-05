7454ce28ba6e93fdf01968362db5cb0834c00e17
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2024-05-04 21:04:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ def draw_cd_diagram(df_perf=None, alpha=0.05, title=None, labels=False):
         }
     if title:
         plt.title(title,fontdict=font, y=0.9, x=0.5)
-    plt.savefig(f'cd-diagram-{csv_data_path[-4]}.png',bbox_inches='tight')
+    plt.savefig(f'cd-diagram-{csv_data_path[:-4]}.png',bbox_inches='tight')
 
 def wilcoxon_holm(alpha=0.05, df_perf=None):
     """
